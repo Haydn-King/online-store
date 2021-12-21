81e7e3d8723f17b48be0c54fe88e675376096f58
Haydn King <ahmillikin@gmail.com>
2021-12-20 16:20:20 -0800

    Third Day - Added More Functionality

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,6 +9,26 @@ router.get("/products", adminController.getProducts); // /admin/products
 
 router.get("/products/new", adminController.getNewProduct);
 
-router.post("/products", imageUploadMiddleware, adminController.createNewProduct);
+router.post(
+  "/products",
+  imageUploadMiddleware,
+  adminController.createNewProduct
+);
+
+router.get(
+  "/products/:id",
+  adminController.getUpdateProduct
+);
+
+router.post(
+  "/products/:id",
+  imageUploadMiddleware,
+  adminController.updateProduct
+);
+
+router.delete(
+  "/products/:id",
+  adminController.deleteProduct
+);
 
 module.exports = router;
